6d2c669016c6deb5998712fd74f27096c25816cc
Markus Nullmeier <mnullmei@ari.uni-heidelberg.de>
2017-02-14 17:55:08 +0100

    fix bounding_box_gist regression test

--- sql/bounding_box_gist.sql
+++ sql/bounding_box_gist.sql
@@ -1,3 +1,4 @@
+SET enable_seqscan=true;
 CREATE TABLE bbox_ellipse (e sellipse not null);
 INSERT INTO bbox_ellipse VALUES ('<{10d, 0.1d}, (0d,0d), 0d>');
 SELECT spoint '(5d, 0d)' @ sellipse '<{10d, 0.1d}, (0d,0d), 0d>' AS inside;
@@ -9,6 +10,7 @@ ANALYZE bbox_ellipse;
 SET enable_seqscan=false;
 SELECT COUNT(*) FROM bbox_ellipse WHERE spoint '(5d, 0d)' @ e;
 
+SET enable_seqscan=true;
 CREATE TABLE bbox_poly (p spoly not null);
 INSERT INTO bbox_poly VALUES ('{(40d,-40d), (0d,80d), (-40d,-40d)}');
 SELECT spoint '(0d, 0d)' @ spoly '{(40d,-40d), (0d,80d), (-40d,-40d)}' AS inside;
@@ -18,6 +20,7 @@ ANALYZE bbox_poly;
 SET enable_seqscan=false;
 SELECT COUNT(*) FROM bbox_poly WHERE spoint '(0d, 0d)' @ p;
 
+SET enable_seqscan=true;
 CREATE TABLE bbox_path (p spath not null);
 INSERT INTO bbox_path VALUES ('{(-46d,0d), (-45d,80d), (-45d,0d), (80d,0d)}');
 SELECT sline(spoint '(0d, -10d)', spoint '(0d, 10d)') && spath '{(-46d,0d), (-45d,80d), (-45d,0d), (80d,0d)}' AS crossing;
